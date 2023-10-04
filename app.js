eb08275c9cf8c6c98728383e2b7b11bd3e3a0536
Till Taeubrich <till.taeubrich@gmail.com>
2023-10-04 19:10:23 +0200

    .

--- app.js
+++ app.js
@@ -10,3 +10,13 @@ app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`server started on port ${PORT}`)
 })
+
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
+app.get('/version', (req, res) => {
+  throw 'error...'
+  // eslint-disable-next-line no-unreachable
+  res.send('4') // change this string to ensure a new version deployed
+})
